edb64845ca8bf30d10bce0f409a47b8ac79e1a43
Benroberts37 <benrobsemail@gmail.com>
2022-02-25 13:12:57 -0800

    Getting a little closer... have 4/9 tests passing

--- src/App.js
+++ src/App.js
@@ -1,105 +1,27 @@
-import React, {useState, useEffect} from "react";
-import {Route, Switch} from 'react-router-dom'
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+import React from 'react'
+import {BrowserRouter as Route, Link} from 'react-router-dom'
 
-import pizzaForm from './Components/PizzaForm'
-import FormSchema from './Components/FormSchema'
+import Pizza from "./Components/Pizza";
 
-import axios from 'axios'
-import * as yup from 'yup'
-
-const initialvalues = {
-  name: '',
-  size: '',
-  sauce: '',
-  bacon: false,
-  chicken: false,
-  grilled_onions: false,
-  grilled_peppers: false,
-}
-
-const initialFormErrors = {
-  name:'',
-  size:'',
-  sauce:'',
-}
-
-const initialOrders=[]
-const initialDisabled = true;
 
 const App = () => {
 
-  const [formData, setFormData] = useState(initialvalues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [orders, setOrders] = useState (initialOrders);
-  const [disabled, setDisabled] = useState(initialDisabled)
-
-
-  const getOrder = getOrder => {
-    axios.post('https://reqres.in/api/orders', getOrder)
-    .then(res => {
-      setOrders(res.data)
-    })
-    .catch(err => {
-      console.error(err)
-    })
-  }
-
-
-    const validate = (name, value) => {
-      yup.reach(FormSchema, name)
-        .validate(value)
-        .then(() => setFormErrors({...formErrors, [name]: ''}))
-        .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
-    }
-
-    const inputChange = (name, value) => {
-      validate(name, value)
-      setFormData({...formData, [name]: value})
-    }
-
-    const formSubmit = () => {
-      const newPizza = {
-        name: formData.name,
-        size: formData.size,
-        sauce: formData.sauce,
-        toppings: [
-          'chicken',
-          'bacon',
-          'grilled_peppers',
-          'grilled_onions'
-        ].filter(topping => formData[topping])
-      }
-      getOrder(newPizza)
-    }
-
-    useEffect(() => {
-      FormSchema.isValid(formData).then(valid => setDisabled(!valid))
-    }, [formData])
-
   return (
-    <>
-    <Router>
-      <nav>
-        <Link id='order-pizza' to='/'>Home</Link>
-        <Link to='/pizza'>Form</Link>
-      </nav>
-        <Route exact path='/pizza'>
-          <div>
-            <h1>Lambda Eats</h1>
-          </div>
-        </Route>
-        <Route path='/pizza'>
-          <pizzaForm
-          formData={formData}
-          change={inputChange}
-          submit={formSubmit}
-          errors={formErrors}
-          disabled={disabled}
-          />
-        </Route>
-    </Router>
-    </>
-  );
+    <div>
+      <Route exact path ='/'>
+        <div>
+          <h1>Lambda Eats</h1>
+        </div>
+        <Link to='/pizza'>
+          <button id='order-pizza'>Want some pizza?</button>
+        </Link>
+      </Route>
+      <Route path='/pizza'>
+        <Pizza></Pizza>
+      </Route>
+    </div>
+  
+  )
 };
+
 export default App;
---
+++ src/Components/Pizza.jsx
@@ -0,0 +1,184 @@
+import React, {useState} from 'react';
+import axios from 'axios'
+import * as yup from 'yup'
+
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .required('Must enter a name')
+        .min(2, 'name must be at least 2 characters'),
+    sauce: yup
+        .string()
+        .oneOf(['red sauce', 'buffalo', 'garlic']),
+    size: yup
+        .string()
+        .oneOf(['small', 'medium', 'large', 'extra large']),
+    chicken: yup.boolean(),
+    bacon: yup.boolean(),
+    grilled_peppers: yup.boolean(),
+    grilled_onions: yup.boolean(),
+    specialInstructions: yup.string()
+})
+
+const Pizza = props => {
+
+    const initialValues = {
+        name: '',
+        size: '',
+        sauce: '',
+        bacon: false,
+        chicken: false,
+        grilled_onions: false,
+        grilled_peppers: false,
+        specialInstructions: '',
+      }
+
+      const [formData, setFormData] = useState(initialValues);
+      const [formErrors, setFormErrors] = useState(initialValues);
+
+      const onChange = evt => {
+          const {name, value, type, checked } = evt.target
+          const useValue = type === 'checkbox' ? checked : value
+
+          yup
+          .reach(formSchema, name)
+          .validate(useValue)
+          .then(valid => {
+              setFormErrors({...formErrors, [name]: ''})
+          })
+          .catch(err => {
+              setFormErrors({
+                  ...formErrors, [name]: err.errors[0]
+              })
+          })
+
+          setFormData({...formData, [name]: useValue})
+      }
+
+      const handleSubmit = evt => {
+          const newOrder = {
+              name: formData.name,
+              size: formData.size,
+              sauce: formData.sauce,
+              bacon: formData.bacon,
+              chicken: formData.chicken,
+              grilled_onions: formData.grilled_onions,
+              grilled_peppers: formData.grilled_peppers,
+              specialInstructions: formData.specialInstructions,
+          }
+          evt.preventDefault()
+
+          axios.post('https://reqres.in/api/orders', newOrder)
+            .then(resp => console.log(resp.data))
+            .catch(err => console.error(err))
+            setFormData(initialValues)
+      }
+
+      return (
+        <div>
+        <h1>Build a pizza!</h1>
+        <form id="pizza-form" onSubmit={handleSubmit}>
+            <label>Name
+                <input
+                    name='name'
+                    type='text'
+                    id='input_name'
+                    onChange={onChange}
+                    value={formData.name}
+                    placeholder='Name'
+                    />
+            </label>
+            <label>Size
+                <select
+                id = 'size-dropdown'
+                name = 'size'
+                value = {formData.size}
+                onChange={onChange}
+                >
+                    <option value = ''>--Pick a size==</option>
+                    <option value = 'Small'>Small</option>
+                    <option value = 'Medium'>Medium</option>
+                    <option value = 'Large'>Large</option>
+                    <option value = 'Extra Large'>Extra Large</option>
+                </select>
+            </label>
+            <h2>Choose a sauce</h2>
+            <label>Red Sauce
+                <input
+                    type = 'radio'
+                    name = 'sauce'
+                    value = 'red sauce'
+                    checked = {formData.sauce === 'red sauce'}
+                    onChange={onChange}
+                    />
+            </label>
+            <label>Buffalo
+                <input
+                    type = 'radio'
+                    name = 'sauce'
+                    value = 'buffalo'
+                    checked = {formData.sauce === 'buffalo'}
+                    onChange={onChange}
+                    />
+            </label>
+            <label>Garlic
+                <input
+                    type = 'radio'
+                    name = 'sauce'
+                    value = 'garlic'
+                    checked = {formData.sauce === 'garlic'}
+                    onChange={onChange}
+                    />
+            </label>
+            <h2>Pick some toppings</h2>
+            <label>Bacon
+                 <input
+                    name = 'bacon'
+                    type = 'checkbox'
+                    checked = {formData.bacon}
+                    onChange = {onChange}
+                />
+            </label>
+            <label>Chicken
+                 <input
+                    name = 'chicken'
+                    type = 'checkbox'
+                    checked = {formData.chicken}
+                    onChange = {onChange}
+                />
+            </label>
+            <label>Grilled Peppers
+                 <input
+                    name = 'grilled_peppers'
+                    type = 'checkbox'
+                    checked = {formData.grilled_peppers}
+                    onChange = {onChange}
+                />
+            </label>
+            <label>Grilled Onions
+                 <input
+                    name = 'grilled_onions'
+                    type = 'checkbox'
+                    checked = {formData.grilled_onions}
+                    onChange = {onChange}
+                />
+            </label>
+            <h2>Submit your order here</h2>
+            <input id='order-button' type = 'submit' />
+            </form>
+            <label>Special instructions
+                <input 
+                type ='text'
+                name = 'special instructions'
+                value = {formData.specialInstructions}
+                id = 'special-text'
+                onChange={(onChange)}
+                />
+            </label>
+
+    </div>
+      )
+}
+
+
+export default Pizza
